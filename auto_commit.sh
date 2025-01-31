767b58eadc70ca1542a0df2be8264566c8269f3c
rianlucascs <rlucasgit@gmail.com>
2025-01-31 15:41:52 -0300

    Commit automático - Fri Jan 31 15:41:52     2025

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Commit automático diário, por exemplo
 
+cd C:\Users\xxis4\Desktop\gerenciamento-ordens-b3
+
 git add .
 git commit -m "Commit automático - $(date)"
 git push origin main

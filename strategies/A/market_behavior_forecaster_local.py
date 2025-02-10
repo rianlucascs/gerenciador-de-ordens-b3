f266bcaedb5d2c2aacdf9dc668d507b46453ffb9
rianlucascs <rlucasgit@gmail.com>
2025-02-10 08:50:08 -0300

    Commit automático - 2025-02-10 08:50:08

--- strategies/A/market_behavior_forecaster_local.py
+++ strategies/A/market_behavior_forecaster_local.py
@@ -1,6 +1,8 @@
 import requests
 import config_model
 from os.path import join, dirname, abspath
+from datetime import date
+import json
 
 # Baixar o script do repositório
 response = requests.get("https://raw.githubusercontent.com/rianlucascs/predicao-dados-binarios/master/api.py")
@@ -22,58 +24,70 @@ mb = MarketBehaviorForecasterLocal(
 
 path = dirname(abspath(__file__))
 
-mb["graphs"](
-    mb["df"]["df"], 
-    "resultado_predicao_acumulado", 
-    (15, 5), 
-    2, 
-    ylabel="resultado_predicao_acumulado", 
-    title=f"Resultado Predição Acumulado - {config_model.TICKER}", 
-    seta=True
-    ).linha().savefig(join(path, "img_resultado_predicao_acumulado"))
+print(mb["df"]["df"])
+
+dias_da_semana = ["segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado", "domingo"]
+
+data = {
+    "Data da atualização" : [str(date.today()), dias_da_semana[date.today().weekday()]],
+    "Última data do preço " : [str(mb["df"]["df"].index[-1])[:10], dias_da_semana[mb["df"]["df"].index[-1].weekday()]],
+    "Data do alvo" : [str(mb["df"]["df"]["date_target"][-1])[:10], dias_da_semana[mb["df"]["df"]["date_target"][-1].weekday()]],
+    "Alvo binário": [int(mb["df"]["df"]["alvo_binario"][-1]), "sell" if int(mb["df"]["df"]["alvo_binario"][-1]) == 0 else "buy"],
+    "Predição": [int(mb["df"]["df"]["predicao"][-1]), "sell" if int(mb["df"]["df"]["predicao"][-1]) == 0 else "buy"]
+}
+
+with open(join(path, "forecast.json"), "w", encoding="utf-8") as arquivo:
+    json.dump(data, arquivo, indent=4, ensure_ascii=False)
+
+# mb["graphs"](
+#     mb["df"]["df"], 
+#     "resultado_predicao_acumulado", 
+#     (15, 5), 
+#     2, 
+#     ylabel="resultado_predicao_acumulado", 
+#     title=f"Resultado Predição Acumulado - {config_model.TICKER}", 
+#     seta=True
+#     ).linha().savefig(join(path, "img_resultado_predicao_acumulado"))
+
+# mb["graphs"](
+#     None, 
+#     None, 
+#     figsize=(10, 5)).comparar_metricas(mb["metrics"]["model"]).savefig(join(path, "img_comparacao_metricas"))
+
+# mb["graphs"](
+#     mb["df"]["df"], 
+#     "resultado_predicao_acumulado", 
+#     (15, 5), 
+#     1, 
+#     ylabel="Retorno Anual (Reais)", 
+#     title="Train, Test and After Test"
+#     ).barplot().savefig(join(path, "img_retorno_anual"))
+
+# mb["graphs"](
+#     mb["df"]["train"][["predicao"]],
+#     "predicao", 
+#     (3, 3), 
+#     xlabel='', 
+#     title=f"Distribuição - Sinais - Predição (Train)",
+#     fontsize_title=10, 
+#     tick_params_labelsize=2).pio().savefig(join(path, "img_distribuicao_sinais_predicao_treino"))
+
+# mb["graphs"](
+#     mb["df"]["test"][["predicao"]],
+#     "predicao", 
+#     (3, 3), 
+#     xlabel='', 
+#     title=f"Distribuição - Sinais - Predição (Test)",
+#     fontsize_title=10, 
+#     tick_params_labelsize=2).pio().savefig(join(path, "img_distribuicao_sinais_predicao_teste"))
+
+# mb["graphs"](
+#     mb["df"]["after_test"][["predicao"]],
+#     "predicao", 
+#     (3, 3), 
+#     xlabel='', 
+#     title=f"Distribuição - Sinais - Predição (After Test)",
+#     fontsize_title=10, 
+#     tick_params_labelsize=2).pio().savefig(join(path, "img_distribuicao_sinais_predicao_apos_o_teste"))
 
 
-mb["graphs"](
-    None, 
-    None, 
-    figsize=(10, 5)).comparar_metricas(mb["metrics"]["model"]).savefig(join(path, "img_comparacao_metricas"))
-
-mb["graphs"](
-    mb["df"]["df"], 
-    "resultado_predicao_acumulado", 
-    (15, 5), 
-    1, 
-    ylabel="Retorno Anual (Reais)", 
-    title="Train, Test and After Test"
-    ).barplot().savefig(join(path, "img_retorno_anual"))
-
-mb["graphs"](
-    mb["df"]["train"][["predicao"]],
-    "predicao", 
-    (3, 3), 
-    xlabel='', 
-    title=f"Distribuição - Sinais - Predição (Train)",
-    fontsize_title=10, 
-    tick_params_labelsize=2).pio().savefig(join(path, "img_distribuicao_sinais_predicao_treino"))
-
-mb["graphs"](
-    mb["df"]["test"][["predicao"]],
-    "predicao", 
-    (3, 3), 
-    xlabel='', 
-    title=f"Distribuição - Sinais - Predição (Test)",
-    fontsize_title=10, 
-    tick_params_labelsize=2).pio().savefig(join(path, "img_distribuicao_sinais_predicao_teste"))
-
-mb["graphs"](
-    mb["df"]["after_test"][["predicao"]],
-    "predicao", 
-    (3, 3), 
-    xlabel='', 
-    title=f"Distribuição - Sinais - Predição (After Test)",
-    fontsize_title=10, 
-    tick_params_labelsize=2).pio().savefig(join(path, "img_distribuicao_sinais_predicao_apos_o_teste"))
-
-html_table = mb["df"]["df"].tail(7).to_html(index=False)
-with open(join(path, "table.html"), "w") as file:
-    file.write(html_table)
